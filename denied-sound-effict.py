9356736723bdb8e888d8df302503ce46d89d40ef
Md Jahirul Islam <65761623+Jahirulovi@users.noreply.github.com>
2022-03-21 10:59:59 -0800

    Create denied-sound-effict.py

---
+++ denied-sound-effict.py
@@ -0,0 +1,19 @@
+import random 
+import os 
+num=random.randint(1,3)
+print(num)
+os.system("clear")
+print("\033[1m\033[33m[\033[32m---Booting--\033[33m]\033[0m")
+
+
+# Condition 
+
+if num == 1:
+    os.system("mpv Access-Denied.mp3")
+
+
+
+else :
+    os.system("mpv Access-Denied.mp3")
+ 
+###########################################
